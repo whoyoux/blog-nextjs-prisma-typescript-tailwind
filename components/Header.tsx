0f90b84faedd172dd97ec47d0b5561b1a7b33ee3
whoyoux <48690153+whoyoux@users.noreply.github.com>
2022-08-19 20:59:53 +0200

    Added about page and edidted a little bit a header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="w-full flex flex-row justify-between items-center my-10">
-      <h1 className="text-3xl">Blog</h1>
-      <h1 className="text-xl">About</h1>
+      <h1 className="text-3xl hover:underline">
+        <Link href="/">Blog</Link>
+      </h1>
+      <h1 className="text-xl hover:underline">
+        <Link href="/about">about</Link>
+      </h1>
     </header>
   );
 };
