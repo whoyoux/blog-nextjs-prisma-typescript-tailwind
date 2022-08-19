1c75f38ee1ab548afdc865462c08ad2872acbbb6
whoyoux <48690153+whoyoux@users.noreply.github.com>
2022-08-19 20:47:37 +0200

    Added 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const NotFoundPage: NextPage = () => {
+  return (
+    <div className="w-full text-center mt-20 text-4xl">
+      404 page not found 🧐
+    </div>
+  );
+};
+
+export default NotFoundPage;
