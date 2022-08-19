89fd4ac3a43b87289b1f246f71e84abf9271dfda
whoyoux <48690153+whoyoux@users.noreply.github.com>
2022-08-19 20:37:59 +0200

    Add global hover style

--- components/FeaturedPost.tsx
+++ components/FeaturedPost.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import dateFormatter from "../lib/dateFormatter";
 
 type FeaturedPostType = {
@@ -16,23 +17,25 @@ const FeaturedPost = ({
   imgUrl,
 }: FeaturedPostType) => {
   return (
-    <article className="bg w-full flex flex-col gap-8 sm:flex-row rounded-lg">
-      <div className="sm:flex-1 pt-8 sm:pt-10 px-10 sm:rounded-l-lg">
-        <h1 className="text-2xl cursor-pointer hover:underline">{title}</h1>
-        <h4 className="text-white/50 text-xs mb-3">
-          {dateFormatter.format(createdAt)}
-        </h4>
-        <p className="line-clamp-3 text-white/70 text-sm">{body}</p>
-      </div>
-      <div className="sm:flex-1 sm:rounded-r-lg px-10 max-w-sm sm:max-w-none mx-auto sm:grid sm:place-content-center sm:py-10">
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={imgUrl}
-          alt="Post image"
-          className="h-auto pb-10 sm:p-0 rounded"
-        />
-      </div>
-    </article>
+    <Link href={`posts/featured`} passHref>
+      <article className="bg w-full flex flex-col gap-8 sm:flex-row rounded-lg hover-outline">
+        <div className="sm:flex-1 pt-8 sm:pt-10 px-10 sm:rounded-l-lg">
+          <h1 className="text-2xl cursor-pointer">{title}</h1>
+          <h4 className="text-white/50 text-xs mb-3">
+            {dateFormatter.format(createdAt)}
+          </h4>
+          <p className="line-clamp-3 text-white/70 text-sm">{body}</p>
+        </div>
+        <div className="sm:flex-1 sm:rounded-r-lg px-10 max-w-sm sm:max-w-none mx-auto sm:grid sm:place-content-center sm:py-10">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={imgUrl}
+            alt="Post image"
+            className="h-auto pb-10 sm:p-0 rounded"
+          />
+        </div>
+      </article>
+    </Link>
   );
 };
 
--- components/RecentPosts.tsx
+++ components/RecentPosts.tsx
@@ -1,16 +1,16 @@
 import { createArray } from "dummy-array";
+import Link from "next/link";
 
 const RecentPosts = () => {
   return (
     <div className="flex sm:flex-row flex-col sm:flex-wrap sm:justify-between w-full mt-20 gap-8 sm:gap-4">
       {createArray({ to: 6 }).map((index: number) => {
         return (
-          <div
-            className="w-full sm:w-[calc(50%-1rem)] bg p-10 rounded-lg"
-            key={index}
-          >
-            Siema {index} &rarr;
-          </div>
+          <Link href={`posts/${index}`} key={index} passHref>
+            <div className="w-full sm:w-[calc(50%-1rem)] bg p-10 rounded-lg hover-outline">
+              Siema {index} &rarr;
+            </div>
+          </Link>
         );
       })}
     </div>
